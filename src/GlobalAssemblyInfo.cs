908d31d3860c52830658e575bf016a9ca3c83fa9
Callon Campbell <calloncampbell@msn.com>
2018-10-15 17:24:04 -0400

    Added nuspec files and updated assembly info.

--- src/GlobalAssemblyInfo.cs
+++ src/GlobalAssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("ReflectSoftware Inc.")]
-[assembly: AssemblyCopyright("Copyright (C) 2018 ReflectSoftware Inc")]
+[assembly: AssemblyCopyright("Copyright (C) 2018 ReflectSoftware Inc.")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: AssemblyInformationalVersion("1.0.0 (rev 0)")]
