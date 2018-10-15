88e63dcf72ecd823abe507b34ada665417dccc85
Callon Campbell <calloncampbell@msn.com>
2018-10-15 15:58:53 -0400

    Updated assembly description.

--- src/WindowsServiceFramework.Extensions/Properties/AssemblyInfo.cs
+++ src/WindowsServiceFramework.Extensions/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 
 [assembly: AssemblyProduct("WindowsServiceFramework.Extensions")]
 [assembly: AssemblyTitle("WindowsServiceFramework.Extensions")]
-[assembly: AssemblyDescription("123")]
+[assembly: AssemblyDescription("A robust service hosting framework for building Windows services using Microsoft .NET Framework.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
